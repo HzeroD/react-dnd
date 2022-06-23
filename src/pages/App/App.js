cbff1c8a2bb79674ee084496001200e2e79bafe1
HzeroD <merkis.ruiz@gmail.com>
2022-06-22 20:33:58 -0400

    Router configuaration for App.js

---
+++ src/pages/App/App.js
@@ -0,0 +1,14 @@
+import logo from '../../logo.svg';
+import './App.css';
+import {}
+
+function App() {
+  return (
+    <>
+      <h1>This is our App!</h1>
+    </>
+  )
+  
+}
+
+export default App;
